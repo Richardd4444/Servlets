7f5592763b666e34d095708b802eca6331aac37d
Richard Mogollón <rmogollon130@unab.edu.co>
2023-01-18 11:11:28 -0500

    Adding RedirectServlet

--- src/com/servlets/RedirectServlet.java
+++ src/com/servlets/RedirectServlet.java
@@ -1,18 +1,18 @@
 package com.servlets;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet("/home")
-public class HomeServlet extends HttpServlet{
+@WebServlet("/redirect")
+public class RedirectServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/html/index.html").forward(req, resp);
+        //redirect to linkedIn
+        resp.sendRedirect("http://www.linkedin.com");
     }
-
 }
